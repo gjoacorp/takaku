bed5dcb987e893f19afe29a89a8aa93a66a8ecaf
eugenekingfish <aojg@protonmail.ch>
2025-08-23 16:46:08 +0100

    [rfr] removed this->... to improve readability

--- src/board.cpp
+++ src/board.cpp
@@ -7,7 +7,7 @@
 board::board(const Vector2& position, const unsigned int& size, const unsigned int& num_players, const std::vector<Color>& colors ) : entity(position) 
 {
   this->size = size;
-  this->num_players_ = num_players;
+  num_players_ = num_players;
   player_colors = colors;
 
   const bool success = init_triangle_shader();
@@ -36,133 +36,133 @@ board::~board()
 
 void board::update(const float& delta) 
 {
-  if (this->initialised)
+  if (initialised)
   {
-    const float time_elapsed = static_cast<float>(this->_timer.time_elapsed());
-    SetShaderValue(triangle_shader_, this->time_uniform_loc, &time_elapsed, SHADER_UNIFORM_FLOAT);
+    const float time_elapsed = static_cast<float>(_timer.time_elapsed());
+    SetShaderValue(triangle_shader_, time_uniform_loc, &time_elapsed, SHADER_UNIFORM_FLOAT);
 
-    for (int i = 0; i < this->circles.size(); ++i) 
+    for (int i = 0; i < circles.size(); ++i) 
     {
       circles[i].update(delta);
-      if ( this->circles[i].is_mouse_over() && !this->game_over && ( this->turn_idx == this->player_idx || !this->ai_enabled) ) 
+      if ( circles[i].is_mouse_over() && !game_over && ( turn_idx == player_idx || !ai_enabled) ) 
       {
-        this->hover_circle = &circles[i];
-        this->circles[i].set_target_radius(this->circles[i].get_mouse_over_growth_mult() * this->circles[i].get_initial_radius());
+        hover_circle = &circles[i];
+        circles[i].set_target_radius(circles[i].get_mouse_over_growth_mult() * circles[i].get_initial_radius());
 
         if ( IsKeyPressed(KEY_F) ) 
         {
-          this->circles[i].set_frozen(!this->circles[i].is_frozen());
+          circles[i].set_frozen(!circles[i].is_frozen());
 
-          if (this->draggable_circle == &this->circles[i])
-            this->draggable_circle = nullptr;
+          if (draggable_circle == &circles[i])
+            draggable_circle = nullptr;
         }
 
         if ( IsMouseButtonPressed(1) ) 
         {
-          if ( !this->circles[i].is_frozen() ) 
-            this->draggable_circle = &this->circles[i];
+          if ( !circles[i].is_frozen() ) 
+            draggable_circle = &circles[i];
         }
-        else if ( IsMouseButtonReleased(1) && this->draggable_circle != nullptr) 
+        else if ( IsMouseButtonReleased(1) && draggable_circle != nullptr) 
           draggable_circle = nullptr;
 
         else if ( IsMouseButtonPressed(0) ) 
         {
-          if ( this->line_source == nullptr ) 
+          if ( line_source == nullptr ) 
           {
-            this->line_source = &this->circles[i];
+            line_source = &circles[i];
           }
           else 
           {
-            if ( this->line_source != &this->circles[i] ) 
-              this->line_target = &this->circles[i];
+            if ( line_source != &circles[i] ) 
+              line_target = &circles[i];
             else
-              this->line_source = nullptr;
+              line_source = nullptr;
           }
 
-          if ( is_move_valid(this->line_source, this->line_target ) )
-            make_move(this->line_source, this->line_target);
+          if ( is_move_valid(line_source, line_target ) )
+            make_move(line_source, line_target);
         }
       }
       else
-        this->circles[i].set_target_radius(this->circles[i].get_initial_radius());
+        circles[i].set_target_radius(circles[i].get_initial_radius());
 
-      if ( &this->circles[i] != this->line_source ) 
+      if ( &circles[i] != line_source ) 
       {
-        if ( this->circles[i].is_frozen() ) 
-          this->circles[i].set_color(this->frozen_circle_color);
+        if ( circles[i].is_frozen() ) 
+          circles[i].set_color(frozen_circle_color);
         else
-          this->circles[i].set_color(this->default_circle_color);
+          circles[i].set_color(default_circle_color);
       }
       else 
       {
-        this->circles[i].set_color( player_colors[turn_idx] );
+        circles[i].set_color( player_colors[turn_idx] );
       }
 
 
-      if (this->draggable_circle != nullptr) 
+      if (draggable_circle != nullptr) 
       {
         Vector2 mp = GetMousePosition();
 
-        if ( ( mp.x > this->get_position().x ) && ( mp.x < this->get_position().x + this->size ) && ( mp.y > this->get_position().y ) && ( mp.y < this->get_position().y + this->size ) )
+        if ( ( mp.x > get_position().x ) && ( mp.x < get_position().x + size ) && ( mp.y > get_position().y ) && ( mp.y < get_position().y + size ) )
         {
-          this->draggable_circle->set_position(GetMousePosition());
-          this->draggable_circle->set_target_position(GetMousePosition());
+          draggable_circle->set_position(GetMousePosition());
+          draggable_circle->set_target_position(GetMousePosition());
         }
         else
-          this->draggable_circle = nullptr;
+          draggable_circle = nullptr;
       }
     }
   }
 
   if (IsKeyPressed(KEY_R)) 
   {
-    this->return_circles_to_initial_positions();
+    return_circles_to_initial_positions();
   }
 }
 
 bool board::simulate_move(circle*& circ_a, circle*& circ_b) 
 {
-  this->lines.push_back( line (circ_a, circ_b, 0.0f, player_colors[this->turn_idx] ) );
+  lines.push_back( line (circ_a, circ_b, 0.0f, player_colors[turn_idx] ) );
   bool losing_move = std::get<0>(contains_monochromatic_triangle());
-  this->lines.pop_back();
+  lines.pop_back();
   return losing_move;
 }
 
 void board::make_move(circle*& circ_a, circle*& circ_b) 
 {
-  this->lines.push_back( line(circ_a, circ_b, this->player_colors[this->turn_idx]) );
-  this->line_counter++;
+  lines.push_back( line(circ_a, circ_b, player_colors[turn_idx]) );
+  line_counter++;
 
   circ_a = nullptr;
   circ_b = nullptr;
 
-  this->mono_tri_data = contains_monochromatic_triangle();
+  mono_tri_data = contains_monochromatic_triangle();
 
-  if (std::get<0>(this->mono_tri_data))
+  if (std::get<0>(mono_tri_data))
   {
-    this->game_over = true;
-    this->losing_player = this->turn_idx;
+    game_over = true;
+    losing_player = turn_idx;
   }
   else
-    this->turn_idx = ( 1 + this->turn_idx ) % this->num_players_;
+    turn_idx = ( 1 + turn_idx ) % num_players_;
 
-  this->_timer.start();
+  _timer.start();
 }
 
 void board::draw() 
 {
   // Drawing the background rectangle of the board
-  DrawRectangleV(this->get_position(), Vector2(this->size, this->size), this->get_color());
+  DrawRectangleV(get_position(), Vector2{ static_cast<float>(size), static_cast<float>(size) }, get_color());
 
-  for (int i = 0; i < this->circles.size(); ++i) 
+  for (int i = 0; i < circles.size(); ++i) 
   {
     circles[i].draw_shadow();
   }
 
-  if ( !this->lines.empty() ) 
+  if ( !lines.empty() ) 
   {
-    for (int i = 0; i < this->line_counter; ++i) 
-    { if ( (this->lines[i].get_source() != nullptr) && (this->lines[i].get_target() != nullptr) ) 
+    for (int i = 0; i < line_counter; ++i) 
+    { if ( (lines[i].get_source() != nullptr) && (lines[i].get_target() != nullptr) ) 
       {
         if ( i == lines.size() - 1 )
           lines[i].set_thickness(12.0f);
@@ -175,10 +175,10 @@ void board::draw()
     }
   }
 
-  if (std::get<0>( this->mono_tri_data) ) 
+  if (std::get<0>( mono_tri_data) ) 
   {
-    const std::vector<circle*> tri_verts = std::get<1>(this->mono_tri_data);
-    Color tri_color = std::get<2>(this->mono_tri_data);
+    const std::vector<circle*> tri_verts = std::get<1>(mono_tri_data);
+    Color tri_color = std::get<2>(mono_tri_data);
     tri_color.a = 200;
 
     BeginShaderMode(triangle_shader_);
@@ -190,7 +190,7 @@ void board::draw()
   }
 
 
-  for (int i = 0; i < this->circles.size(); ++i) 
+  for (int i = 0; i < circles.size(); ++i) 
   {
     circles[i].draw();
   }
@@ -198,8 +198,8 @@ void board::draw()
 
 void board::init_circles(const float& poly_radius, const float& circle_radius, const unsigned int& num_circles) 
 {
-  int centre_x = this->get_position().x + (this->size / 2); 
-  int centre_y = this->get_position().y + (this->size / 2);
+  int centre_x = get_position().x + (size / 2); 
+  int centre_y = get_position().y + (size / 2);
   double angle = (2.0f * 3.14159265358979323846) / (double)num_circles;
 
   for (int i = 0; i < num_circles; ++i) 
@@ -210,33 +210,33 @@ void board::init_circles(const float& poly_radius, const float& circle_radius, c
     int y = centre_y + (poly_radius * std::sin(i * angle));
 
     // We are safe to cast x and y to floats since we do not expect them to be larger than 2^24.
-    this->circle_initial_positions.push_back(Vector2{ static_cast<float>(x), static_cast<float>(y) });
+    circle_initial_positions.push_back(Vector2{ static_cast<float>(x), static_cast<float>(y) });
     c.set_target_position(Vector2{ static_cast<float>(x), static_cast<float>(y)});
     c.set_position(Vector2{ static_cast<float>(centre_x), static_cast<float>(centre_y)});
     c.set_initial_radius(circle_radius);
-    c.set_mouse_over_growth_mult(this->circle_growth_mult);
-    c.set_color(this->default_circle_color);
+    c.set_mouse_over_growth_mult(circle_growth_mult);
+    c.set_color(default_circle_color);
     c.set_frozen(false);
 
-    this->circles.push_back(c);
+    circles.push_back(c);
   }
-  this->_timer.start();
-  this->initialised = true;
+  _timer.start();
+  initialised = true;
 }
 
 void board::move_circles_to(const std::vector<Vector2>& positions)
 {
-  if ( this->circles.size() == positions.size() ) {
-    for ( int i = 0; i < this->circles.size(); i++ ) {
-      if (!this->circles[i].is_frozen())
-        this->circles[i].set_target_position(positions[i]);
+  if ( circles.size() == positions.size() ) {
+    for ( int i = 0; i < circles.size(); i++ ) {
+      if (!circles[i].is_frozen())
+        circles[i].set_target_position(positions[i]);
     }
   }
 }
 
 void board::return_circles_to_initial_positions() 
 {
-  this->move_circles_to(this->circle_initial_positions);
+  move_circles_to(circle_initial_positions);
 }
 
 bool board::are_colors_equal(const Color& col_a, const Color& col_b) const 
@@ -251,26 +251,26 @@ std::pair<bool, line*> board::does_line_exist(circle* c_a, circle* c_b)
   if ( c_a == nullptr || c_b == nullptr) 
     return std::make_pair(false, nullptr);
 
-  for (size_t i = 0; i < this->lines.size(); ++i) {
-    if ( ( this->lines[i].get_source() == c_a && this->lines[i].get_target() == c_b ) ||
-        ( this->lines[i].get_target() == c_a && this->lines[i].get_source() == c_b ) ) 
-      return std::make_pair(true, &this->lines[i]);
+  for (size_t i = 0; i < lines.size(); ++i) {
+    if ( ( lines[i].get_source() == c_a && lines[i].get_target() == c_b ) ||
+        ( lines[i].get_target() == c_a && lines[i].get_source() == c_b ) ) 
+      return std::make_pair(true, &lines[i]);
   }
   return std::make_pair(false, nullptr);
 }
 
 std::tuple<bool, std::vector<circle*>, Color> board::contains_monochromatic_triangle() 
 {
-  for (size_t i = 0; i < this->circles.size(); ++i) {
+  for (size_t i = 0; i < circles.size(); ++i) {
 
-    for (size_t j = i + 1; j < this->circles.size(); ++j) {
+    for (size_t j = i + 1; j < circles.size(); ++j) {
 
-      for (size_t k = j + 1; k < this->circles.size(); ++k) {
+      for (size_t k = j + 1; k < circles.size(); ++k) {
 
         // Obtaining line data
-        const std::pair<bool, line*> line_data_ij = this->does_line_exist(&this->circles[i], &this->circles[j]);
-        const std::pair<bool, line*> line_data_jk = this->does_line_exist(&this->circles[j], &this->circles[k]);
-        const std::pair<bool, line*> line_data_ki = this->does_line_exist(&this->circles[k], &this->circles[i]);
+        const std::pair<bool, line*> line_data_ij = does_line_exist(&circles[i], &circles[j]);
+        const std::pair<bool, line*> line_data_jk = does_line_exist(&circles[j], &circles[k]);
+        const std::pair<bool, line*> line_data_ki = does_line_exist(&circles[k], &circles[i]);
 
         // Verifying that a triangle exists
         if ( line_data_ij.first && line_data_jk.first && line_data_ki.first)
@@ -280,7 +280,7 @@ std::tuple<bool, std::vector<circle*>, Color> board::contains_monochromatic_tria
             const Color tri_color = line_data_ij.second->get_color();
             std::vector<circle*> temp;
 
-            temp.push_back(&this->circles[i]); temp.push_back(&this->circles[j]); temp.push_back(&this->circles[k]); 
+            temp.push_back(&circles[i]); temp.push_back(&circles[j]); temp.push_back(&circles[k]); 
             return std::make_tuple(true, temp, tri_color);
           }
       }
@@ -294,11 +294,11 @@ bool board::is_move_valid(circle*& circ_a, circle*& circ_b) {
   if ( ((circ_a != nullptr) && (circ_b != nullptr)) && (circ_a != circ_b) ) 
   {
     // Checking whether the lines [source -> target] and [target -> source] already exist
-    if ( !this->lines.empty() ) 
+    if ( !lines.empty() ) 
     {
-      for (int i = 0; i < this->line_counter; ++i) 
+      for (int i = 0; i < line_counter; ++i) 
       {
-        if ( ( (this->lines[i].get_source() == circ_a) && (this->lines[i].get_target() == circ_b) ) || ( (this->lines[i].get_source() == circ_b) && (this->lines[i].get_target() == circ_a) ) ) 
+        if ( ( (lines[i].get_source() == circ_a) && (lines[i].get_target() == circ_b) ) || ( (lines[i].get_source() == circ_b) && (lines[i].get_target() == circ_a) ) ) 
           return false;
       }
     }
@@ -314,7 +314,7 @@ bool board::init_triangle_shader()
   triangle_shader_ = LoadShader(0, TextFormat("shaders/rt.fs", GLSL_VERSION));
   if ( IsShaderValid(triangle_shader_) )
   {
-    this->time_uniform_loc = GetShaderLocation(triangle_shader_, "time");
+    time_uniform_loc = GetShaderLocation(triangle_shader_, "time");
     return true;
   }
   return false;
@@ -322,47 +322,47 @@ bool board::init_triangle_shader()
 
 void board::reset_board() 
 {
-  this->turn_idx = 0;
-  this->lines.clear();
-  this->line_source = nullptr;
-  this->line_target = nullptr;
-  this->draggable_circle = nullptr;
-  this->line_counter = 0;
-  this->game_over = false;
-  this->losing_player = -1;
-  this->mono_tri_data = std::make_tuple(false, std::vector<circle*>{}, BLACK);
-  this->_timer.start();
+  turn_idx = 0;
+  lines.clear();
+  line_source = nullptr;
+  line_target = nullptr;
+  draggable_circle = nullptr;
+  line_counter = 0;
+  game_over = false;
+  losing_player = -1;
+  mono_tri_data = std::make_tuple(false, std::vector<circle*>{}, BLACK);
+  _timer.start();
 
-  for (size_t i = 0; i < this->circles.size(); ++i)
-    this->circles[i].kill_outgoing_lines();
+  for (size_t i = 0; i < circles.size(); ++i)
+    circles[i].kill_outgoing_lines();
 }
 
 void board::hard_reset_board() 
 {
-  this->reset_board();
-  this->circles.clear();
-  this->initialised = false;
-  this->circle_initial_positions.clear();
+  reset_board();
+  circles.clear();
+  initialised = false;
+  circle_initial_positions.clear();
 }
 
 void board::thaw_circles() 
 {
-  for ( auto c : this->circles )
+  for ( auto c : circles )
     c.set_frozen(false);
 }
 
 // Getters and setters
 
-unsigned int board::get_line_counter() const { return this->line_counter; }
+unsigned int board::get_line_counter() const { return line_counter; }
 
-std::vector<circle>& board::get_circles() { return this->circles; }
+std::vector<circle>& board::get_circles() { return circles; }
 
-unsigned int board::get_turn_idx() const { return this->turn_idx; }
+unsigned int board::get_turn_idx() const { return turn_idx; }
 
-bool board::is_game_over() const { return this->game_over; }
+bool board::is_game_over() const { return game_over; }
 
-int board::get_losing_player() const { return this->losing_player; }
+int board::get_losing_player() const { return losing_player; }
 
-bool board::is_initialised() const { return this->initialised; }
+bool board::is_initialised() const { return initialised; }
 
-double board::get_time_since_last_move() const { return this->_timer.time_elapsed(); }
+double board::get_time_since_last_move() const { return _timer.time_elapsed(); }
